b3892ed7bda746acd3fd9426d8d0a999d208a518
nknkansc <162102802+nknkansc@users.noreply.github.com>
2025-07-18 23:01:59 +0530

    Add server/models/User.js 1752859919251

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long']
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', UserSchema);
